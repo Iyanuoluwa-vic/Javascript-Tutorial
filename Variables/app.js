9aa3afa243ffa96edb0fc91a49ffcd3390120b7c
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-16 18:26:07 -0500

    Javascript variable

---
+++ Variables/app.js
@@ -0,0 +1,16 @@
+let firstName = "Johns+";
+
+let address, city, state, zip;
+
+address = "123 Main St";
+city = "New York";
+state = "NY";
+console.log(firstName);
+
+console.log(firstName);
+
+console.log(firstName);
+
+console.log(firstName);
+
+console.log(address, city, state);
