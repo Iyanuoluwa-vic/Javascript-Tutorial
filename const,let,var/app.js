4d5ac30da8df9a842ea90afaf8cc97faf063e0dd
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-16 18:33:31 -0500

    const,let,var

---
+++ const,let,var/app.js
@@ -0,0 +1,8 @@
+// const is used to declare a variable that can't be changed
+const lastName = "Smith";
+
+console.log(lastName);
+
+// let is used to declare a variable that can be changed
+let firstName = "John";
+console.log(firstName);
