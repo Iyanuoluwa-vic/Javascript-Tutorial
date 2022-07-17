b1c3fe7069665dedee9c5cc5ddf56dfa81758261
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-17 17:22:01 -0500

    fuctions

---
+++ fuctions/app.js
@@ -0,0 +1,10 @@
+// creating a fuction
+function hello() {
+  console.log("Hello james");
+  console.log("Hello peter");
+  console.log("Hello kike");
+}
+
+//  invoke the function
+hello();
+hello();
