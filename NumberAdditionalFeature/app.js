1081deea084c7640d29492e94b150d5509ede84b
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-16 19:34:00 -0500

    number additional feature

---
+++ NumberAdditionalFeature/app.js
@@ -0,0 +1,15 @@
+// += -= /= *=
+// += he addition assignment operator (+=) adds the value of the right operand to a variable and assigns the result to the variable.
+let number = 10;
+number += 5;
+console.log(number);
+
+// ++  he increment operator (++) increments (adds one to) its operand and returns a value.
+let number2 = 3;
+number2++;
+console.log(number2);
+
+// % he remainder operator (%) returns the remainder of a division operation.
+let a = 10;
+let b = 3;
+console.log(a % b);
