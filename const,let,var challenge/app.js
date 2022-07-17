dd5e7548adedf4dde6d15e571a34c585c7d5bac2
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-17 18:12:01 -0500

    fuction challenge

--- const,let,var challenge/app.js
+++ const,let,var challenge/app.js
@@ -77,3 +77,33 @@ fruits[fruits.length - 1] = "kiwi";
 
 console.log(fruits);
 console.log(firstFruit);
+
+// ## Functions #5
+
+// 1. create "calculateTotal" function
+// 2. add two parameters subTotal, tax
+// 3. return sum of parameters
+// 4. create 3 vars "order1","order2","order3"
+// 5. call calculateTotal, pass in some values and assign result to each order
+// 6. log all three orders
+// 7. refactor "calculateTotal" to function expression
+
+function calculateTotal(subTotal, tax) {
+  return subTotal + tax;
+}
+
+const order1 = calculateTotal(100, 10);
+const order2 = calculateTotal(200, 20);
+const order3 = calculateTotal(300, 30);
+
+console.log(order1, order2, order3);
+
+//  changing it to a fuction expression
+const total = function (subTotal, tax) {
+  return subTotal + tax;
+};
+
+const order4 = total(100, 10);
+const order5 = total(200, 20);
+const order6 = total(300, 30);
+console.log(order4, order5, order6);
