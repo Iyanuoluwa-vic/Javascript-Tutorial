de003a8967cdfe05276202bb22ebb7259bc61f98
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-17 17:38:21 -0500

    returning from a function

--- fuctions/app.js
+++ fuctions/app.js
@@ -13,5 +13,14 @@ hello();
 function greet(name) {
   console.log("hello there " + name);
 }
-
 greet("james");
+
+// returning from the function call
+width = 12;
+height = 123;
+
+function area(width, height) {
+  return (area = width * height);
+}
+
+console.log("calculated area is " + area(width, height));
