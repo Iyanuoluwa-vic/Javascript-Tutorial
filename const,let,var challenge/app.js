a8ce7187bdc0ce4cdc61fe170073ae1d17e321f4
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-16 19:42:00 -0500

    ## Numbers #3

--- const,let,var challenge/app.js
+++ const,let,var challenge/app.js
@@ -4,16 +4,13 @@ const lastName = "Smith";
 
 let address = "123 Main Street";
 address = "first street";
-
 console.log(firstName, lastName, address);
 
 // ## Concatenate Strings #2
 
 const street = "123's Main Street";
 const country = "USA";
-
 const fullMailingAddress = street + " " + country;
-
 console.log(fullMailingAddress);
 
 // ## Numbers #3
@@ -22,15 +19,19 @@ const score1 = 50;
 const score2 = 42;
 const score3 = 39;
 
-const averageScore = (score1 + score2 + score3) / 3;
 const totalScore = score1 + score2 + score3;
+const averageScore = (score1 + score2 + score3) / 3;
 
 console.log(averageScore);
 console.log(totalScore);
 
-const plates = 10;
+const plates = 11;
 const people = 2;
-let remainingPlate = plates - people;
-remainingPlate = remainingPlate + 1;
-
+let remainingPlate = plates % people;
 console.log(remainingPlate);
+
+remainingPlate++;
+console.log(remainingPlate);
+
+const message = "There are " + remainingPlate + " plates left";
+console.log(message);
