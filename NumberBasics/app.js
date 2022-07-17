d111f3c0432dc996b745c6d546d2b4919ba24983
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-16 19:18:27 -0500

    number basics

---
+++ NumberBasics/app.js
@@ -0,0 +1,15 @@
+// numbers are lossly typed in javascript it doesnt declare the type of the number it just stores it as a number
+// when creating number we do not put quotes around it because it is a number
+
+// not like java where you have to declare the type of the number
+const number = 10;
+let number2 = 20.3;
+let number3 = 20.3;
+number2 = "it is loosely typed";
+
+const add = number + number3;
+
+console.log(number);
+console.log(number2);
+
+console.log(add);
