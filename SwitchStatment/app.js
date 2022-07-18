97aec4b230a4131439688ccfbc8e56ecea03135e
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-17 23:40:37 -0500

    switch statment

---
+++ SwitchStatment/app.js
@@ -0,0 +1,15 @@
+const number = 3;
+
+switch (number) {
+  case 1:
+    console.log("1");
+    break;
+  case 2:
+    console.log("2");
+    break;
+  case 3:
+    console.log("3");
+    break;
+  default:
+    alert("Invalid");
+}
