8a52d8a54de62418cb707ff34254f3bd43bdb8fa
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-17 23:29:56 -0500

    conditional statments

---
+++ conditional statment/app.js
@@ -0,0 +1,10 @@
+// conditional statment
+const num1 = 10;
+const num2 = 20;
+if (num2 > num1) {
+  console.log(num2 + " is greater than " + num1);
+} else if (num2 < num1) {
+  console.log(num2 + " is not greater than " + num1);
+} else {
+  console.log("it is equal");
+}
