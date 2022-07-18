46c5b48ebcb4ba9e1d29ae334a23952d6f9918ca
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-07-17 23:35:22 -0500

    logical operator

---
+++ LogicalOperator/app.js
@@ -0,0 +1,11 @@
+// logical operator
+// (&&) and (||)
+
+const name1 = "iyanu";
+const name2 = "james";
+
+if (name1 == "iyanu" && name2 == "james") {
+  console.log("it is equal");
+} else {
+  console.log("it is not equal");
+}
